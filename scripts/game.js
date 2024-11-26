f430fad75f975210b4f3d6247e29b375f456cdc0
Lorraine Scanlon <lorrainescanlon@gmail.com>
2024-11-26 22:35:21 +0000

    added tests and functions for player sequence

--- scripts/game.js
+++ scripts/game.js
@@ -2,6 +2,7 @@ let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
+    turnNumber: 0,
     choices: ["button1", "button2", "button3", "button4"]
 };
 
@@ -9,6 +10,18 @@ function newGame() {
     game.score = 0;
     game.playerMoves = [];
     game.currentGame = [];
+    for (let circle of document.getElementsByClassName("circle")) {
+        if (circle.getAttribute("data-listener") !== "true") {
+            circle.addEventListener("click", (e) => {
+                let move = e.target.getAttribute("id");
+                    lightsOn(move);
+                    game.playerMoves.push(move);
+                    playerTurn();
+                
+            });
+            circle.setAttribute("data-listener", "true");
+        }
+    }
     showScore();
     addTurn();
 };
@@ -20,7 +33,7 @@ function showScore() {
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    // showTurns();    
+    showTurns();    
 }
 
 function lightsOn(circ) {
@@ -29,4 +42,33 @@ function lightsOn(circ) {
         document.getElementById(circ).classList.remove("light");
     }, 400);
 }
-module.exports = {game, newGame, showScore, addTurn, lightsOn};
+
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length){
+            clearInterval(turns);
+        }
+    }, 800)
+};
+
+function playerTurn() {
+    let i = game.playerMoves.length - 1;
+    if (game.currentGame[i] === game.playerMoves[i]) {
+        if (game.currentGame.length == game.playerMoves.length) {
+            game.score++;
+            showScore();
+            addTurn();
+        }
+
+    } else {
+        alert("Wrong move!");
+        newGame();
+    }
+
+}
+
+module.exports = {game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn};
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,8 +2,9 @@
  * @jest-environment jsdom
  */
 
-const {game, newGame, showScore, addTurn, lightsOn} = require("../game");
+const {game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn} = require("../game");
 
+jest.spyOn(window, "alert").mockImplementation(() =>{} );
 
 beforeAll(() => {
     let fs = require("fs");
@@ -28,6 +29,9 @@ describe("game object contains correct keys", () => {
     });
     test("choices contains the correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+    test("turnNumber key exists", () => {
+        expect("turnNumber" in game).toBe(true);
     });
 });
 
@@ -77,4 +81,26 @@ describe("gameplay works correctly", () => {
         lightsOn(game.currentGame[0]);
         expect(button.classList).toContain("light");
     });
+    test("showTurns should update game.turnNumber", () => {
+        game.turnNumber = 42;
+        showTurns();
+        expect(game.turnNumber).toBe(0);
+    });
+    test("expect data-listener to be true", () => {
+        const elements = document.getElementsByClassName("circle");
+            for (let element of elements) {
+                expect(element.getAttribute("data-listener")).toEqual("true");
+            };
+    });
+    test("should increment score if turn is correct", () => {
+        game.playerMoves.push(game.currentGame[0]);
+        playerTurn();
+        expect(game.score).toBe(1);
+    })
+    test("should call an alert if the move is wrong", () => {
+        game.playerMoves.push("wrong");
+        playerTurn();
+        expect(window.alert).toBeCalledWith("Wrong move!");
+    })
 });
+
